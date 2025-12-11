9334d2215509ccd7bc7749851f040813f0d39fc0
Makda0-0 <makdas205@gmail.com>
2025-12-11 14:21:15 +0300

    Area

---
+++ Week3/area.py
@@ -0,0 +1,9 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.area = width * height 
+
+r1 = Rectangle(2, 4)
+print(r1.area)  
+
